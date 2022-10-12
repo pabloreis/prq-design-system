1683ddacdb6ed4302d0141e75f9c3810686c5ba3
Pablo <pabloreiss@gmail.com>
2022-10-12 18:53:37 +0200

    feat: create Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,30 @@
+import { Slot } from "@radix-ui/react-slot";
+import { clsx } from "clsx";
+import { ReactNode } from "react";
+
+export interface ButtonProps {
+  children: ReactNode;
+  asChild?: boolean;
+  isFullWidth?: boolean;
+}
+
+export function Button({
+  children,
+  asChild,
+  isFullWidth = false,
+}: ButtonProps) {
+  const Comp = asChild ? Slot : "button";
+
+  return (
+    <Comp
+      className={clsx(
+        "py-4 px-3 bg-cyan-500 rounded font-semibold text-black text-sm transition-colors hover:bg-cyan-300 focus:ring-2 ring-white",
+        {
+          "w-full": isFullWidth,
+        }
+      )}
+    >
+      {children}
+    </Comp>
+  );
+}
